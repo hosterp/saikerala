6b58806dce30c078c80e0a1282fb84078cfeb8e4
Libiya <libiyasebastianhiworth@gmail.com>
2024-03-21 15:21:41 +0530

    districtlevel meeting changes added

--- sesa_requirement/models/districtlevel.py
+++ sesa_requirement/models/districtlevel.py
@@ -5,11 +5,25 @@ class DistrictLevelMeetings(models.Model):
 
     date = fields.Datetime(string="Date", default=lambda self: fields.Date.context_today(self))
     events_planned = fields.Text(string="Programs/Events Planned")
-    no_monthily_meeting = fields.Integer(string="Monthly Meetings")
+    no_monthily_meeting = fields.Integer(string="Meetings")
     no_family = fields.Integer(string='No. of Families Attend Family Narayana Seva')
     no_saplings = fields.Integer(string='No. of Saplings Planted (Prematharu)')
     missing_activities = fields.Integer(string='Missing Activities')
     remark= fields.Text(string='Remarks')
+    event_district = fields.Selection([('AL', 'ALAPPUZHA'),
+                                       ('ER', 'ERNAKULAM'),
+                                       ('ID', 'IDUKKI'),
+                                       ('KN', 'KANNUR'),
+                                       ('KS', 'KASARAGOD'),
+                                       ('KL', 'KOLLAM'),
+                                       ('KT', 'KOTTAYAM'),
+                                       ('KZ', 'KOZHIKODE'),
+                                       ('MA', 'MALAPPURAM'),
+                                       ('PL', 'PALAKKAD'),
+                                       ('PT', 'PATHANAMTHITTA'),
+                                       ('TV', 'THIRUVANANTHAPURAM'),
+                                       ('TS', 'THRISSUR'),
+                                       ('WA', 'WAYANAD')])
 
     @api.multi
     def print_activity_district_level_meeting_details_report(self):
